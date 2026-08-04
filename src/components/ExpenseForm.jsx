28e5c1a18808a9125106aaa249e281c81f559eb0
Nelson Jimenez <nelson@Nelsons-MacBook-Pro.local>
2026-08-04 13:30:23 -0400

    Fix expense account selection

--- src/components/ExpenseForm.jsx
+++ src/components/ExpenseForm.jsx
@@ -102,7 +102,7 @@ export default function ExpenseForm({
             <option value="">Sélectionner un compte</option>
 
             {accounts.map((account) => (
-              <option key={account.id} value={account.name}>
+              <option key={account.id} value={account.id}>
                 {account.name}
               </option>
             ))}
